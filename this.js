26c3d96735bf4d8e0c0869820fa131979c892fcb
sahishnusuresh <sahishnusuresh@gmail.com>
2023-01-16 17:32:05 +0530

    trying this

---
+++ this.js
@@ -0,0 +1,13 @@
+//Understanding this keyword
+ var obj={
+    name:'sahi',
+    age:56
+ };
+ console.log(obj.name)
+var obj1={
+   name:'sahishnu',
+   printname:function(){
+      console.log(this.name);
+   }
+};
+obj1.printname();
