d25720748a6b7e0e040434c890c42d730f8aef33
sahishnusuresh <sahishnusuresh@gmail.com>
2023-01-17 12:37:53 +0530

    async and await1

--- asynchronous/asyncawait.js
+++ asynchronous/asyncawait.js
@@ -1,10 +1,10 @@
 //Async tells JS the function will return a promise.
 //Instead of using chianing thens, await will execute the function when the 
 //promise is resolved.
-// async function tellscore(score){
-//     return score/100;
-// } 
-// tellscore(10).then((val)=>{console.log(val)});
+async function tellscore(score){
+    return score/100;
+} 
+tellscore(10).then((val)=>{console.log(val)});
 
 async function tellingscore(score){
 var func=new Promise((res,rej)=>{
